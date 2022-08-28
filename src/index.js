90529e50ebd7f37c3a7f9623e6760331094ca6c2
Volodymyr Lysenko <lusiy321@gmail.com>
2022-08-28 14:49:30 +0300

    Modal and mobile menu

--- src/index.js
+++ src/index.js
@@ -13,3 +13,36 @@
     body.classList.toggle('no-scroll');
   }
 })();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    body: document.querySelector('[data-page]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-1]'),
+    closeModalBtn: document.querySelector('[data-modal-close-1]'),
+    modal: document.querySelector('[data-modal-1]'),
+    body: document.querySelector('[data-page-1]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
