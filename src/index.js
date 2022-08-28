941b54613d83f44c340da5a497e051db8685c9cb
Volodymyr Lysenko <lusiy321@gmail.com>
2022-08-28 20:30:41 +0300

    debug

--- src/index.js
+++ src/index.js
@@ -30,12 +30,30 @@
     refs.body.classList.toggle('no-scroll');
   }
 })();
+
 (() => {
   const refs = {
     openModalBtn: document.querySelector('[data-modal-open-1]'),
-    closeModalBtn: document.querySelector('[data-modal-close-1]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal-1]'),
     body: document.querySelector('[data-page-1]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.body.classList.toggle('no-scroll');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open-2]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal-2]'),
+    body: document.querySelector('[data-page-2]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
